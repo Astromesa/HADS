14f1b9bd0c6b2cf148d1c2ae205fc94eb3b6232f
Astromesa <105600426+Astromesa@users.noreply.github.com>
2022-05-25 12:29:48 +0200

    Update simple_mesa_plotter.py

--- simple_mesa_plotter.py
+++ simple_mesa_plotter.py
@@ -6,4 +6,10 @@ h = mr.MesaData('../mesa_work/LOGS/history.data')
 plt.xlabel('Star age (yr)')
 plt.ylabel('logLuminosity log(L/Lsun)')
 plt.plot(h.star_age, h.log_L)
+print("preshow")
 plt.show()
+print("postshow")
+plt.savefig('plot1.jpg', dpi=300)
+plt.close()
+
+
